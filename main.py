6e130a6c687e9279d0d6a84bfec6fe9f2e8acd72
AndrewHiew <andrewyy30@gmail.com>
2024-09-14 15:29:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 dfs = tabula.read_pdf("GOAL.pdf", pages="all", multiple_tables=True)
 
-# Iterate through each table extracted
 for i, df in enumerate(dfs):
     df = df.fillna(method='ffill')
     
@@ -12,7 +11,6 @@ for i, df in enumerate(dfs):
         df.columns = [' '.join(df.iloc[0:2, col].astype(str).values).strip() for col in range(df.shape[1])]
         df = df.iloc[2:] 
 
-    # Save each table as an individual CSV
     csv_file_name = f"table_{i+1}.csv"
     df.to_csv(csv_file_name, index=False)
     
